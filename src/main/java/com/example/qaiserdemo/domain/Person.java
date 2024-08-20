641c7bd65f0ed5725a5231162f0dea000147c674
Jilani <rjilani@teksystems.com>
2024-08-19 19:19:05 -0500

    updated to Spring boot 3.2.8

--- src/main/java/com/example/qaiserdemo/domain/Person.java
+++ src/main/java/com/example/qaiserdemo/domain/Person.java
@@ -1,8 +1,10 @@
 package com.example.qaiserdemo.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
 @Entity
